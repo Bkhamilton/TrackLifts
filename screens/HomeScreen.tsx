561e92457d511c529b11697beb5b7ddeded27aac
Bkhamilton <bkhamilton310@gmail.com>
2025-05-12 18:38:28 -0400

    added addroutinemodal and newExerciseModal back into home page, fixed errors in code

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -5,18 +5,47 @@ import { StatusBar, StyleSheet, TouchableOpacity } from 'react-native';
 import RoutineInfo from '@/components/Home/RoutineInfo';
 import Title from '../components/Title';
 
+import AddRoutineModal from '@/components/modals/AddRoutineModal';
+import RoutineOptions from '@/components/modals/RoutineOptions';
 import { ScrollView, View } from '@/components/Themed';
 import { DBContext } from '@/contexts/DBContext';
 import useHookHome from '@/hooks/useHookHome';
+import { Exercise } from '@/utils/types';
 
 export default function HomeScreen() {
 
-    const { closeRoutineModal, openRoutineModal, openAddRoutineModal, openRoutineOptionsModal } = useHookHome();
+    const { 
+        addRoutineModal,
+        closeAddRoutineModal,
+        routineOptionsModal,
+        closeRoutineOptionsModal,
+        closeRoutineModal, 
+        openRoutineModal, 
+        openAddRoutineModal, 
+        openRoutineOptionsModal,
+    } = useHookHome();
 
     const { db, routines } = useContext(DBContext) 
 
+    const onAdd = (routine: { title: string; exercises: Exercise[] }) => {
+        console.log('Add Routine:', routine);
+    }
+
     return (
         <View style={styles.container}>
+            <AddRoutineModal
+                visible={addRoutineModal}
+                close={closeAddRoutineModal}
+                add={onAdd}
+            />
+            <RoutineOptions
+                visible={routineOptionsModal}
+                close={closeRoutineOptionsModal}
+                routine={{ id: 1, title: 'Test Routine', exercises: [] }} 
+                onDelete={(id) => {
+                    console.log('Delete Routine with ID:', id);
+                }}
+            />
             <View style={{top:60}}>
                 <Title title="TrackLifts"></Title>
                 <TouchableOpacity
