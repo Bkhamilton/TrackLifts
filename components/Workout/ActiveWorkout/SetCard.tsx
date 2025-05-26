f751f02c4f58275567104d41deaeb20c35e3aee5
Bkhamilton <bkhamilton310@gmail.com>
2025-05-26 17:21:46 -0400

    changed View background to transparent

--- components/Workout/ActiveWorkout/SetCard.tsx
+++ components/Workout/ActiveWorkout/SetCard.tsx
@@ -88,6 +88,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flex: 1,
         justifyContent: 'center',
+        backgroundColor: 'transparent',
     },
     input: {
         borderWidth: 1,
